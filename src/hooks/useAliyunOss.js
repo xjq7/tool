dfe850d793f22aa1ec5c6f116825d12c2cd57b22
xjq <lebnizjoan@gmail.com>
2021-12-20 00:38:55 +0800

    feat: 支持批量上传，修复oss token过期问题

--- src/hooks/useAliyunOss.js
+++ src/hooks/useAliyunOss.js
@@ -2,36 +2,41 @@
 import { useState, useEffect, useCallback } from 'react';
 import AliyunOss from 'ali-oss';
 import { getUploadToken } from '@/api';
+
+const EXPIRES = 1000 * 60 * 10;
+
 function useAliyunOss() {
   const [ossToken, setOssToken] = useState();
   const [instance, setInstance] = useState();
 
-  const fetchToken = () => {
-    return getUploadToken().then(res => {
+  const fetchToken = useCallback(() => {
+    return getUploadToken().then((res) => {
       setOssToken(res);
       return res;
     });
-  };
+  }, []);
 
   const initAliOss = useCallback(() => {
+    if (!ossToken) return;
     const { AccessKeyId, AccessKeySecret, SecurityToken } = ossToken || {};
     const store = new AliyunOss({
       ...ossToken,
       stsToken: SecurityToken,
       accessKeyId: AccessKeyId,
-      accessKeySecret: AccessKeySecret
+      accessKeySecret: AccessKeySecret,
+      refreshSTSTokenInterval: EXPIRES,
+      refreshSTSToken: () => fetchToken(),
     });
     setInstance(store);
-  }, [ossToken]);
+  }, [ossToken, fetchToken]);
 
   useEffect(() => {
-    if (ossToken) {
-      initAliOss();
-    }
-  }, [ossToken, initAliOss]);
+    initAliOss();
+  }, [initAliOss]);
+
   useEffect(() => {
     fetchToken();
-  }, []);
+  }, [fetchToken]);
 
   return { ossStore: instance, ossToken, fetchToken };
 }
--- src/pages/img/component/UploadAliyun.js
+++ src/pages/img/component/UploadAliyun.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Upload, message, Spin } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
@@ -9,36 +9,53 @@ const { Dragger } = Upload;
 const UploadAliyun = ({ onChange }) => {
   const [loading, setLoading] = useState(false);
 
+  const uploadQueueRef = useRef(new Set());
+
   const { ossStore } = useAliyunOss();
 
-  const upload = ({ file }) => {
+  const upload = (r) => {
+    if (loading && uploadQueueRef.current.size === 6) {
+      message.error('最多同时上传 5 个文件!');
+    }
+
+    if (uploadQueueRef.current.size > 5) {
+      return;
+    }
+
+    if (!loading) setLoading(true);
+
+    const { file } = r;
     const { name } = file;
     const date = new Date();
     const YY = date.getFullYear();
     const MM = date.getMonth() + 1;
     const DD = date.getDate();
     let folder = `/${YY}/${MM}/${DD}/`;
-    let reNameAsDate = `${YY}.${MM}.${DD}-`;
-    setLoading(true);
+    let reNameAsDate = `${Date.now()}_`;
+    uploadQueueRef.current.add(file.uid);
+
     ossStore
       .put(`${folder}${reNameAsDate}${name}`, file)
-      .then(res => {
+      .then((res) => {
         console.log(res);
         const { url } = res;
         message.success(`${name}上传成功!`);
         onChange({ name, url });
       })
-      .catch(err => {
+      .catch((err) => {
         message.error(`${name}上传失败`);
       })
       .finally(() => {
-        setLoading(false);
+        uploadQueueRef.current.delete(file.uid);
+        if (uploadQueueRef.current.size === 0) {
+          setLoading(false);
+        }
       });
   };
 
   const uploadParams = {
     name: 'file',
-    multiple: false,
+    multiple: true,
     showUploadList: false,
     customRequest: upload,
     beforeUpload(file) {
@@ -47,7 +64,7 @@ const UploadAliyun = ({ onChange }) => {
         message.error('请选择小于10 mb 的文件');
         return false;
       }
-    }
+    },
   };
   return (
     <Spin spinning={loading} tip="上传中...">
@@ -62,7 +79,7 @@ const UploadAliyun = ({ onChange }) => {
 };
 
 UploadAliyun.propTypes = {
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
 };
 
 export default UploadAliyun;
